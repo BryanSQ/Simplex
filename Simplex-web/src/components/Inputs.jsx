f40bde499cb31dccd83a9fea655059a117e58674
Bryan Josue Sandí Quirós <brysq@estudiantec.cr>
2024-04-19 22:34:28 -0600

    [UPDATE] handle slack, artificial and surplus variables
    - Handle more variables
    - Refactor simplex preprocess functions
    - Refactor UI

--- Simplex-web/src/components/Inputs.jsx
+++ Simplex-web/src/components/Inputs.jsx
@@ -1,17 +1,121 @@
-import { useSelector } from "react-redux"
-import Restrictions from "./Restrictions"
-import Variables from "./Variables"
+import { useDispatch, useSelector } from "react-redux"
+import { setRestrictions, setVariables } from "../reducers/simplexReducer"
+import Simplex from "../simplex"
+
+import '../styles/Variables.css'
+import '../styles/Restrictions.css'
 
 const Inputs = () => {
+  const dispatch = useDispatch()
+  const variables = useSelector(state => state.config.variables)
+  const restricciones = useSelector(state => state.config.restricciones)
   const objetivo = useSelector(state => state.config.objetivo)
 
+  const handleVariablesSubmit = (vars) => {    
+    // get data from inputs
+    const inputs = vars
+
+    const variables = []
+    inputs.forEach((input, key) => {
+      let var_object = {}
+      var_object[`x${key + 1}`] = input.value
+      variables.push(var_object)
+    })
+    dispatch(setVariables(variables))
+  }
+
+  const handleRestrictionsSubmit = (restrictions) => {
+    // get data from inputs
+    const inputs = restrictions
+
+    const rests = []
+    let k = 0
+    for (let i = 0; i < restricciones; i++) {
+      const rest = {}
+      for (let j = 0; j < Number(variables) + 2; j++) {
+        if (inputs[k].value === '>=' || inputs[k].value === '<=' || inputs[k].value === '=') {
+          rest['comp'] = inputs[k].value
+        }
+        else if (j === Number(variables) + 1) {
+          rest['res'] = inputs[k].value
+        }
+        else {
+          rest[`x${j + 1}`] = inputs[k].value
+        }
+        k++
+      }
+      rests.push(rest)
+    }
+    //console.log(rests)
+    dispatch(setRestrictions(rests))
+    Simplex()
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // filter inputs to get variables and restrictions
+    const inputs = Array.from(e.target.elements)
+      .filter(input => input.id.includes('input-x') || input.id.includes('input-restriccion') || input.id.includes('select') || input.id.includes('input-res'))
+
+    // get variables
+    const variables = inputs.filter(input => input.id.includes('input-x'))
+
+    // get restrictions
+    const rests = inputs.filter(input => input.id.includes('input-restriccion') || input.id.includes('select') || input.id.includes('input-res'))
+    
+    handleVariablesSubmit(variables)
+    handleRestrictionsSubmit(rests)
+  };
+
   return (
-    <div>      
+    <div>
       <div>
-        <Variables />
-      </div>
-      <div>
-        <Restrictions />
+        <div id="variables-container">
+          <h2>Variables</h2>
+          <div>
+            <form onSubmit={handleSubmit}>
+              {
+                Array.from({ length: variables }, (_, i) => (
+
+                  <span key={i}>
+                    <input id={`input-x${i}`} type="number" />
+                    <label htmlFor={`input-x${i}`}>X{i + 1}</label>
+                  </span>
+                ))
+              }
+              <br />
+              <div id="restrictions-container">
+                <h2>Restricciones</h2>
+                <div>
+                  {
+                    Array.from({ length: restricciones }, (_, i) => (
+                      <span key={i} className="restriction-row">
+                        {
+                          Array.from({ length: variables }, (_, j) => (
+                            <span key={j} className="restriction-item">
+                              <input id={`input-restriccion-${i}-${j}`} type="number" />
+                              <label htmlFor={`input-restriccion-${i}-${j}`}>X{j + 1}</label>
+                            </span>
+                          ))
+                        }
+                        <select id={`select-${i}`} className="equality">
+                          <option value="<=">{"<="}</option>
+                          <option value=">=">{">="}</option>
+                          <option value="=">{"="}</option>
+                        </select>
+                        <input type="number" id={`input-res${i + 1}`} />
+                      </span>
+                    ))
+                  }
+                </div>
+              </div>
+              <div>
+                <button type="submit">Guardar</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
       <p>Objetivo: {objetivo}</p>
     </div>
--- Simplex-web/src/simplex.js
+++ Simplex-web/src/simplex.js
@@ -3,22 +3,34 @@ import Store from "./store";
 const Simplex = () => {
     // access the simplex reducer
     const {variables, restrictions} = Store.getState().simplex;
-    console.log(restrictions);
-     buildRestrictions(restrictions);
 
     // create a matrix with the simplex data and BVS
+
     const {BVS, matrix, header} = buildMatrix(variables, restrictions);
     
 
-    console.log(BVS, matrix);
+    console.log(BVS, matrix, header);
 
     const processedMatrix = simplexProcess(matrix, BVS, header);
 
-    console.log('After simplex', processedMatrix);
-
-    //console.log(variables, restrictions);
+    showMatrix(processedMatrix, BVS, header);
+    showResults(processedMatrix, BVS);
 }
 
+const showMatrix = (matrix, BVS, header) => {
+    const tableHeader = ['i', 'BVS', ...header, 'RHS'];
+    const table = [tableHeader, ...matrix.map((row, i) => [i, BVS[i], ...row])];
+    console.table(table);
+}
+
+const showResults = (matrix, BVS) => {
+    const results = {}
+    for (let i = 0; i < matrix.length; i++) {
+        results[BVS[i]] = matrix[i][matrix[i].length - 1];
+    }
+    console.log(results);
+
+}
 
 const buildZ = (variables, rows) =>{
     const z = variables.map((variable) => {
@@ -36,26 +48,69 @@ const buildRestrictions = (restrictions) => {
     // if the restriction is a <=, we need to add a slack variable to the matrix
     // if the restriction is a =, we need to add a artificial variable to the matrix
     // if the restriction is a >=, we need to add a surplus variable to the matrix
-    for (let i = 0; i < restrictions.length; i++) {
-        const restriction = restrictions[i];
-        const restrictionValue = Number(restriction[Object.keys(restriction)[0]]);
-        const restrictionType = Object.keys(restriction)[0];
-        if (restrictionType === '<=') {
-            restrictions[i] = {...restriction, s: 1, a: 0, r: restrictionValue} //[0,0,0,0,1]
-        } else if (restrictionType === '=') {
-            restrictions[i] = {...restriction, s: 0, a: 1, r: restrictionValue} //[0,0,0,0,0,0, 1]
-        } else {
-            restrictions[i] = {...restriction, s: -1, a: 1, r: restrictionValue} //[0,0,0,0,0,0, -1, 1]
+    const { slackCount, artificialCount } = getOtherVariablesCount(restrictions);
+    const restrictionMatrix = [];
+
+    let slackPos = 0;
+    let artificialPos = 0;
+    for (let restriction of restrictions){
+
+        const row = []
+        const slack = Array.from({length: slackCount}, () => 0);
+        const artificial = Array.from({length: artificialCount}, () => 0);
+
+        // get the variables x1, x2, x3, etc values
+        const values = Object.keys(restriction)
+            .filter(key => key !== 'comp' && key !== 'res')
+            .map(key => Number(restriction[key]));
+
+        row.push(...values);
+        // get the restriction type
+        const comp = restriction.comp;
+
+        if (comp === '<=') {
+            slack[slackPos] = 1;
+            slackPos++;
         }
+        if (comp === '=') {
+            artificial[artificialPos] = 1;
+            artificialPos++;            
+        }
+        if (comp === '>=') {
+            slack[slackPos] = -1;
+            artificial[artificialPos] = 1;
+            slackPos++;
+            artificialPos++;
+        }
+        row.push(...slack);
+        row.push(...artificial);
+        row.push(Number(restriction.res));
+        restrictionMatrix.push(row);                
     }
-    console.log(restrictions);
+    return restrictionMatrix;
 }
 
-const buildBVS = (variables, restrictions) => {
+const getOtherVariablesCount = (restrictions) => {
+    let surplus = restrictions.filter(restriction => restriction.comp === '>=').length;
+    let slackCount = restrictions.filter(restriction => restriction.comp === '<=').length + surplus;        
+    let artificialCount = restrictions.filter(restriction => restriction.comp === '=').length + surplus;
+    return {
+        slackCount,
+        artificialCount        
+    }
+}
+
+
+const buildBVS = (variables, { slackCount, artificialCount }) => {
     let start = variables + 1;
     const bvs = []
-    for (let i = 0; i < restrictions; i++) {
+
+    for (let i = 0; i < slackCount; i++) {
         bvs.push(`s${start}`)
+        start++;
+    }
+    for (let i = 0; i < artificialCount; i++) {
+        bvs.push(`a${start}`)
         start++;
     }
     return ['z', ...bvs];
@@ -63,14 +118,15 @@ const buildBVS = (variables, restrictions) => {
 
 const buildMatrix = (variables, restrictions) => {
     const matrix = [];
-    const BVS = buildBVS(variables.length, restrictions.length);
-    const rows = restrictions.length + 1;
+    const { slackCount, artificialCount } = getOtherVariablesCount(restrictions);
+    const BVS = buildBVS(variables.length, { slackCount, artificialCount });
+    const rows = slackCount + artificialCount + 1;
 
     const z = buildZ(variables, rows);
     matrix.push(z);
-    const slack = buildSlack(restrictions);
+    const restrictionMatrix = buildRestrictions(restrictions);
     
-    matrix.push(...slack);
+    matrix.push(...restrictionMatrix);
 
     const header = []
     header.push(...variables.map((variable) => Object.keys(variable)[0]));
@@ -91,8 +147,6 @@ const simplexProcess = (matrix, BVS, header) => {
         const pivotRow = findPivotRow(matrix, pivot);
         BVS[pivotRow.index] = header[pivot];
         const pivotValue = matrix[pivotRow.index][pivot];
-        console.log(pivotValue);
-        console.log('Before iteration', matrix);
         matrix = iteration(matrix, pivotRow.index, pivot, pivotValue);
     }
     return matrix;
@@ -122,7 +176,9 @@ const iteration = (matrix, pivotRow, pivotCol, pivotValue) =>{
     for (let i = 0; i < matrix.length; i++) {
         if (i !== pivotRow && matrix[i][pivotCol] !== 0) {
             matrix = rowAddition(matrix, pivotRow, i, -matrix[i][pivotCol]);
-            console.log(matrix);
+
+            // salvar resultados para la tabla
+            // console.log(matrix);
         }      
     }
     return matrix;
